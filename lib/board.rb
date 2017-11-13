4221c86ba3fec73eefb9806bc789a52e3f3fa067
Jibreel Hamid-Diaz <shuckle1028@gmail.com>
2017-11-13 14:02:46 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,38 @@
 # Define the variable board below.
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+def Hallelujah_Lyrics
+  puts "Well I heard there was a secret chord
+That David played and it pleased the Lord
+But you don't really care for music, do you?
+Well it goes like this: the fourth, the fifth
+The minor fall and the major lift
+The baffled king composing Hallelujah
+Hallelujah [x4]
+Your faith was strong but you needed proof
+You saw her bathing on the roof
+Her beauty and the moonlight overthrew you
+She tied you to her kitchen chair
+She broke your throne and she cut your hair
+And from your lips she drew the Hallelujah
+Hallelujah [x4]
+Baby I've been here before
+I've seen this room and I've walked this floor (you know)
+I used to live alone before I knew you
+And I've seen your flag on the marble arch
+And love is not a victory march
+It's a cold and it's a broken Hallelujah
+Hallelujah [x4]
+There was a time when you let me know
+What's really going on below
+But now you never show that to me, do you?
+But remember when I moved in you
+And the holy dove was moving too
+And every breath we drew was Hallelujah
+Hallelujah [x4]
+Maybe there's a God above
+All I've ever learned from love
+Was how to shoot somebody who outdrew you
+And it's not a cry that you hear at night
+It's not somebody who's seen the light
+It's a cold and it's a broken Hallelujah
+Hallelujah [x13]"
+end
